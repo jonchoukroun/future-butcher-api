e00b7f4d45157bf6a91817c6d18c2b241d84382a
Jon C <jonchoukroun@gmail.com>
2018-06-22 15:23:30 +0000

    Config ssl for prod

--- config/prod.exs
+++ config/prod.exs
@@ -1,19 +1,5 @@
 use Mix.Config
 
-config :future_butcher_api, FutureButcherApiWeb.Endpoint,
-  http: [port: 4000],
-  url: [host: "futurebutcher.com", port: 80],
-  https: [
-    port: 443,
-    keyfile: Path.expand("/etc/letsencrypt/live/futurebutcher.com/privkey.pem"),
-    certfile: Path.expand("/etc/letsencrypt/live/futurebutcher.com/fullchain.pem")
-  ],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  server: true,
-  root: ".",
-  code_reloader: false,
-  version: Application.spec(:future_butcher_api, :vsn)
-
 # Do not print debug messages in production
 config :logger, level: :info
 
