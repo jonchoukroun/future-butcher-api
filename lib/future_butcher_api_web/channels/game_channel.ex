0c0d14f2c099894e99adb14f5d6e2fcfd02116cc
Jon <jonchoukroun@gmail.com>
2018-12-16 10:23:24 -0800

    Capture socket join errors

--- lib/future_butcher_api_web/channels/game_channel.ex
+++ lib/future_butcher_api_web/channels/game_channel.ex
@@ -13,7 +13,9 @@ defmodule FutureButcherApiWeb.GameChannel do
       retrieve_player(payload)
       {:ok, %{hash_id: hash_id}, socket}
     else
-      {:error, %{reason: "Unauthorized"}}
+      msg = "Unauthorized"
+      Sentry.capture_message("Failed to re-join socket", extra %{extra: msg})
+      {:error, %{reason: msg}}
     end
   end
 
@@ -24,12 +26,16 @@ defmodule FutureButcherApiWeb.GameChannel do
       player = persist_player(payload)
       {:ok, %{hash_id: player.hash_id}, socket}
     else
-      {:error, %{reason: "Unauthorized"}}
+      msg = "Unauthorized"
+      Sentry.capture_message("Failed to join socket", extra %{extra: msg})
+      {:error, %{reason: msg}}
     end
   end
 
-    def join("game:" <> _player, _payload) do
-    {:error, %{reason: "Invalid payload"}}
+  def join("game:" <> _player, _payload) do
+    msg = "Invalid payload"
+    Sentry.capture_message("Failed to join game", extra %{extra: msg})
+    {:error, %{reason: msg}}
   end
 
   def handle_info({:after_join, screen_name}, socket) do
