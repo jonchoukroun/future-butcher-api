752b16e8c7ae3f469cdef537079d98261bdcd594
Jon <jonchoukroun@gmail.com>
2019-11-12 19:55:39 -0800

    Restore endpoint ssl

--- config/releases.exs
+++ config/releases.exs
@@ -1,9 +1,17 @@
 import Config
 
+ssl_cert_path = System.fetch_env!("SSL_CERT_PATH")
+
 config :future_butcher_api, FutureButcherApiWeb.Endpoint,
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
-  http: [port: System.fetch_env!("APP_PORT")],
+  http: [port: 8888],
   url: [host: "api.futurebutcher.com"],
+  https: [
+    port: String.to_integer(System.fetch_env!("APP_PORT")),
+    keyfile: Path.absname("#{ssl_cert_path}privkey.pem"),
+    certfile: Path.absname("#{ssl_cert_path}cert.pem")
+  ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
   check_origin: false,
   server: true
 
