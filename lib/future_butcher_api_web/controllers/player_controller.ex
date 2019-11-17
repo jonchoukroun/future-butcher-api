3eb5ab97765fef33eb9727aac04d74154d8cc753
Jon <jonchoukroun@gmail.com>
2019-11-17 00:31:55 -0800

    Render jwt on successful player creation and signing

--- lib/future_butcher_api_web/controllers/player_controller.ex
+++ lib/future_butcher_api_web/controllers/player_controller.ex
@@ -1,14 +1,14 @@
 defmodule FutureButcherApiWeb.PlayerController do
   use FutureButcherApiWeb, :controller
 
-  alias FutureButcherApi.{Auth, Player}
+  alias FutureButcherApi.{Auth, Player, Guardian}
 
+  @spec create(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def create(conn, %{"player" => player_params}) do
-    with {:ok, %Player{} = player} <- Auth.create_player(player_params) do
+    with {:ok, %Player{} = player} <- Auth.create_player(player_params),
+         {:ok, token, _claims} <- Guardian.encode_and_sign(player) do
       conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.player_path(conn, :show, player))
-      |> render("show.json", player: player)
+      |> render("jwt.json", %{jwt: token})
     end
   end
 end
--- lib/future_butcher_api_web/views/player_view.ex
+++ lib/future_butcher_api_web/views/player_view.ex
@@ -2,15 +2,5 @@ defmodule FutureButcherApiWeb.PlayerView do
   use FutureButcherApiWeb, :view
   alias FutureButcherApiWeb.PlayerView
 
-  def render("index.json", %{players: players}) do
-    %{data: render_many(players, PlayerView, "player.json")}
-  end
-
-  def render("show.json", %{player: player}) do
-    %{data: render_one(player, PlayerView, "player.json")}
-  end
-
-  def render("player.json", %{player: player}) do
-    %{id: player.id}
-  end
+  def render("jwt.json", %{jwt: jwt}), do: %{jwt: jwt}
 end
