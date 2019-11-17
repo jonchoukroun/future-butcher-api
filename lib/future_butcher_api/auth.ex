57a2af6920a1718a5c290446be4801d1f0947d39
Jon <jonchoukroun@gmail.com>
2019-11-17 10:34:34 -0800

    Implement player authentication from name and password

--- lib/future_butcher_api/auth.ex
+++ lib/future_butcher_api/auth.ex
@@ -5,6 +5,7 @@ defmodule FutureButcherApi.Auth do
 
   import Ecto.Query, warn: false
   alias FutureButcherApi.{Repo, Player}
+  alias FutureButcherApi.Guardian
 
   @type player() :: %Player{}
   @type changeset() :: Ecto.Changeset
@@ -72,4 +73,45 @@ defmodule FutureButcherApi.Auth do
   """
   @spec delete_player(player()) :: {:ok, player()} | {:error, :atom}
   def delete_player(%Player{} = player), do: Repo.delete(player)
+
+  @spec token_sign_in(String.t(), String.t()) :: String.t() | {:error, :unauthorized}
+  def token_sign_in(name, password) do
+    case name_password_auth(name, password) do
+      {:ok, player} ->
+        Guardian.encode_and_sign(player)
+
+      _ ->
+        {:error, :unauthorized}
+    end
+  end
+
+  defp get_by_name(name) when is_binary(name) do
+    Player
+    |> preload([:scores])
+    |> Repo.get_by(name: name)
+    |> case do
+      nil ->
+        Comeonin.Argon2.dummy_checkpw()
+        {:error, "Login error."}
+      player ->
+        {:ok, player}
+    end
+  end
+  defp get_by_name(_), do: {:error, :invalid_credentials}
+
+  defp verify_password(password, %Player{} = player) when is_binary(password) do
+    if Argon2.check_pass(player, password) do
+      {:ok, player}
+    else
+      {:error, :invalid_password}
+    end
+  end
+  defp verify_password(_password), do: {:error, :invalid_password}
+
+  defp name_password_auth(name, password) when is_binary(name) and is_binary(password) do
+    with {:ok, player} <- get_by_name(name) do
+      verify_password(password, player)
+    end
+  end
+  defp name_password_auth(_name, _password), do: {:error, :invalid_credentials}
 end
