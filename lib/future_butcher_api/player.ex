8efd9fd984818e722cfd6c4018ec21dad88819c4
Jon <jonchoukroun@gmail.com>
2019-11-16 23:44:38 -0800

    Add password hashing to player schema

--- lib/future_butcher_api/player.ex
+++ lib/future_butcher_api/player.ex
@@ -6,8 +6,11 @@ defmodule FutureButcherApi.Player do
 
   schema "players" do
     field :email, :string
-    field :hash_id, :string
     field :name, :string
+    field :password_hash, :string
+
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     has_many :scores, Score
 
@@ -17,10 +20,23 @@ defmodule FutureButcherApi.Player do
   @doc false
   def changeset(player, attrs) do
     player
-    |> cast(attrs, [:name, :email])
-    |> unique_constraint(:hash_id)
+    |> cast(attrs, [:name, :email, :password, :password_confirmation])
+    |> validate_required([:name, :password, :password_confirmation])
+    |> unique_constraint(:name)
     |> unique_constraint(:email)
-    |> validate_required([:name])
     |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 6)
+    |> validate_confirmation(:password)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, Argon2.add_hash(password))
+
+      _ ->
+        changeset
+    end
   end
 end
