10a1c75d016eb06bb820831b04e37d802e0ca6ab
Jon <jonchoukroun@gmail.com>
2019-11-17 00:09:23 -0800

    Fix argon password hasher

--- lib/future_butcher_api/player.ex
+++ lib/future_butcher_api/player.ex
@@ -30,13 +30,9 @@ defmodule FutureButcherApi.Player do
     |> put_password_hash()
   end
 
-  defp put_password_hash(changeset) do
-    case changeset do
-      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
-        put_change(changeset, :password_hash, Argon2.add_hash(password))
-
-      _ ->
-        changeset
-    end
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
   end
+
+  defp put_password_hash(changeset), do: changeset
 end
