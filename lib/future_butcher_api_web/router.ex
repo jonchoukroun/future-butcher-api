03bf047c7b8e1e2a8e5c2908dce9e672984c9b52
Jon <jonchoukroun@gmail.com>
2019-11-18 09:08:33 -0800

    Implement protected routes using guardian pipeline

--- lib/future_butcher_api_web/router.ex
+++ lib/future_butcher_api_web/router.ex
@@ -3,15 +3,29 @@ defmodule FutureButcherApiWeb.Router do
   use Plug.ErrorHandler
   use Sentry.Plug
 
+  alias FutureButcherApi.Guardian.ApiAuthPipeline
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :api_protected do
+    plug ApiAuthPipeline
   end
 
   scope "/api/v1", FutureButcherApiWeb do
     pipe_through :api
 
-    resources "/sign_up", Auth.RegistrationController, only: [:create]
+    post "/sign_up", Auth.PlayerController, :create
 
     post "/sign_in", Auth.SessionsController, :create
+  end
+
+  scope "/api/v1", FutureButcherApiWeb do
+    pipe_through [:api, :api_protected]
+
+    delete "/sign_out", Auth.SessionsController, :delete
+
+    get "/current_player", Auth.PlayerController, :show
   end
 end
