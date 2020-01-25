242b9bb26f4ee533872707798774d755040b9281
Jon <jonchoukroun@gmail.com>
2020-01-25 11:07:48 -0800

    Update http port envs

--- config/releases.exs
+++ config/releases.exs
@@ -4,10 +4,10 @@ ssl_cert_path = System.fetch_env!("SSL_CERT_PATH")
 
 config :future_butcher_api, FutureButcherApiWeb.Endpoint,
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
-  http: [port: 8888],
+  http: [port: System.fetch_env!("HTTP_PORT")],
   url: [host: "api.futurebutcher.com"],
   https: [
-    port: String.to_integer(System.fetch_env!("APP_PORT")),
+    port: String.to_integer(System.fetch_env!("HTTPS_PORT")),
     keyfile: Path.absname("#{ssl_cert_path}privkey.pem"),
     certfile: Path.absname("#{ssl_cert_path}fullchain.pem")
   ],
